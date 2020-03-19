abfff1a0936b3489dd0f15d330c7a7f437eedee3
nmuncy <nathan.muncy1@gmail.com>
2020-03-18 19:41:18 -0600

    udpated for blip naming correction

--- BIDS_step1_dcm2nii.sh
+++ BIDS_step1_dcm2nii.sh
@@ -14,23 +14,30 @@
 # 3) Will require input for constructing dataset_description.json
 #
 # 4) Make sure to update the jq task input where the epi Json files are being input!
+#
+# 5) If an experiment has two phases (Study, Test), where Study has 2 runs and Test 4,
+#		then: expPhaseRuns=(2 4)
+#		and Task runs 1-2 are Study, 3-6 are Test.
 
 
 
 
 ###??? change these variables/arrays
 rawDir=/Volumes/Yorick/MriRawData							# location of raw data
-workDir=/Volumes/Yorick/STT_reml							# desired working directory
+workDir=/Volumes/Yorick/Temporal							# desired working directory
 tempDir=/Volumes/Yorick/Templates
 
-subjList=(1295 1859 1875 1949 2439 2575 3013 3018 3023 3031 3042 3043 3046 3075 3223 3425 3710 3711 3712 3735 3746 3761 3762 3763 3767 3768 3775 3778 3783 3786 3816 3849 3850 3851 3852)									# list of subjects
-session=STT													# scanning session - for raw data organization (ses-STT)
-task=task-stt												# name of task, for epi data naming
+subjList=(`cat ${workDir}/Experiment3/subjList.txt`)		# a column of subjects (e.g. sub-111 sub-112 sub-113)
+session=Temporal											# scanning session - for raw data organization (ses-STT)
+task=TemporalMST											# name of task, for epi data naming
 
-epiDirs=(STUDY TEST1{1,2} TEST2{1..4})						# epi dicom directory name/prefix
+epiDirs=(Temporal_B{1..3})									# epi dicom directory name/prefix
 t1Dir=t1													# t1 ditto
 t2Dir=HHR													# t2 ditto
-blipDir=()													# blip ditto - names one per scanning phase
+
+blipDir=(Reverse_Blip)										# blip ditto - names one per scanning phase
+blipEnc=PA 													# alphanumeric string for blip direction
+expPhaseRuns=(3)											# Note 5
 
 
 
@@ -70,7 +77,7 @@ if [ ! -s ${workDir}/rawdata/dataset_description.json ]; then
 	read -p 'Please enter title of the manuscript:' title
 
 	echo -e "\n\nNote: authors must be within quotes and separated by a comma & space"
-	echo -e "\te.g. \"Nate Muncy\", \"Brock Kirwan\"\n"
+	echo -e "\te.g. \"Nate M. Muncy\", \"C. Brock Kirwan\"\n"
 	read -p 'Please enter authors:' authors
 
 	cat > ${workDir}/rawdata/dataset_description.json << EOF
@@ -89,11 +96,12 @@ fi
 for i in ${subjList[@]}; do
 
 	### set up BIDS data dirs
-	anatDir=${workDir}/rawdata/sub-${i}/anat
-	funcDir=${workDir}/rawdata/sub-${i}/func
-	derivDir=${workDir}/derivatives/sub-${i}
+	anatDir=${workDir}/rawdata/${i}/anat
+	funcDir=${workDir}/rawdata/${i}/func
+	fmapDir=${workDir}/rawdata/${i}/fmap
+	derivDir=${workDir}/derivatives/${i}
 
-	for j in {anat,func,deriv}Dir; do
+	for j in {anat,func,fmap,deriv}Dir; do
 		if [ ! -d $(eval echo \${$j}) ]; then
 			mkdir -p $(eval echo \${$j})
 		fi
@@ -101,25 +109,25 @@ for i in ${subjList[@]}; do
 
 
 	### construct data
-	dataDir=${rawDir}/sub-${i}/ses-${session}/dicom
+	dataDir=${rawDir}/${i}/ses-${session}/dicom
 
 	# t1 data
-	if [ ! -f ${anatDir}/sub-${i}_T1w.nii.gz ]; then
+	if [ ! -f ${anatDir}/${i}_T1w.nii.gz ]; then
 
-		dcm2niix -b y -ba y -z y -o $anatDir -f tmp_sub-${i}_T1w ${dataDir}/${t1Dir}*/
+		dcm2niix -b y -ba y -z y -o $anatDir -f tmp_${i}_T1w ${dataDir}/${t1Dir}*/
 
 		# Deface by Brock Kirwan
-		3dAllineate -base ${anatDir}/tmp_sub-${i}_T1w.nii.gz -input ${tempDir}/mean_reg2mean.nii.gz -prefix ${anatDir}/tmp_mean_reg2mean_aligned.nii -1Dmatrix_save ${anatDir}/tmp_allineate_matrix
-		3dAllineate -base ${anatDir}/tmp_sub-${i}_T1w.nii.gz -input ${tempDir}/facemask.nii.gz -prefix ${anatDir}/tmp_facemask_aligned.nii -1Dmatrix_apply ${anatDir}/tmp_allineate_matrix.aff12.1D
-		3dcalc -a ${anatDir}/tmp_facemask_aligned.nii -b ${anatDir}/tmp_sub-${i}_T1w.nii.gz -prefix ${anatDir}/sub-${i}_T1w.nii.gz -expr "step(a)*b"
-		mv ${anatDir}/tmp_sub-${i}_T1w.json ${anatDir}/sub-${i}_T1w.json
+		3dAllineate -base ${anatDir}/tmp_${i}_T1w.nii.gz -input ${tempDir}/mean_reg2mean.nii.gz -prefix ${anatDir}/tmp_mean_reg2mean_aligned.nii -1Dmatrix_save ${anatDir}/tmp_allineate_matrix
+		3dAllineate -base ${anatDir}/tmp_${i}_T1w.nii.gz -input ${tempDir}/facemask.nii.gz -prefix ${anatDir}/tmp_facemask_aligned.nii -1Dmatrix_apply ${anatDir}/tmp_allineate_matrix.aff12.1D
+		3dcalc -a ${anatDir}/tmp_facemask_aligned.nii -b ${anatDir}/tmp_${i}_T1w.nii.gz -prefix ${anatDir}/${i}_T1w.nii.gz -expr "step(a)*b"
+		mv ${anatDir}/tmp_${i}_T1w.json ${anatDir}/${i}_T1w.json
 		rm ${anatDir}/tmp*
 	fi
 
 
 	# t2
-	if [ ! -f ${anatDir}/sub-${i}_T2w.nii.gz ]; then
-		dcm2niix -b y -ba y -z y -o $anatDir -f sub-${i}_T2w ${dataDir}/${t2Dir}*/
+	if [ ! -f ${anatDir}/${i}_T2w.nii.gz ]; then
+		dcm2niix -b y -ba y -z y -o $anatDir -f ${i}_T2w ${dataDir}/${t2Dir}*/
 	fi
 
 
@@ -128,15 +136,16 @@ for i in ${subjList[@]}; do
 
 		pos=$(($j+1))
 
-		if [ ! -f ${funcDir}/sub-${i}_${task}_run-${pos}_bold.nii.gz ]; then
-			dcm2niix -b y -ba y -z y -o $funcDir -f sub-${i}_${task}_run-${pos}_bold ${dataDir}/${epiDirs[$j]}*/
+		if [ ! -f ${funcDir}/${i}_task-${task}_run-${pos}_bold.nii.gz ]; then
+			dcm2niix -b y -ba y -z y -o $funcDir -f ${i}_task-${task}_run-${pos}_bold ${dataDir}/${epiDirs[$j]}*/
 		fi
 
 		# Json append by Brock Kirwan
-		funcJson=${funcDir}/sub-${i}_${task}_run-${pos}_bold.json
+		# updated by Nate to receive argument
+		funcJson=${funcDir}/${i}_task-${task}_run-${pos}_bold.json
 		taskExist=$(cat $funcJson | jq '.TaskName')
 		if [ "$taskExist" == "null" ]; then
-			jq '. |= . + {"TaskName":"STT"}' $funcJson > ${derivDir}/tasknameadd.json         ###??? replace "STT" with your task name
+			jq --arg holdTask "$task" '. |= . + {"TaskName":$holdTask}' $funcJson > ${derivDir}/tasknameadd.json
 			rm $funcJson && mv ${derivDir}/tasknameadd.json $funcJson
 		fi
 	done
@@ -144,10 +153,32 @@ for i in ${subjList[@]}; do
 
 	# blip
 	if [ ! -z $blipDir ]; then
+
+		runningC=0
 		for k in ${!blipDir[@]}; do
+
 			pos=$(($k+1))
-			if [ ! -f ${funcDir}/sub-${i}_phase-${pos}_blip.nii.gz ]; then
-				dcm2niix -b y -ba y -z y -o $funcDir -f sub-${i}_phase-${pos}_blip ${dataDir}/${blipDir}*/
+			if [ ! -f ${fmapDir}/${i}_ses-${session}_dir-${blipEnc}_run-${pos}_phasediff.nii.gz ]; then
+				dcm2niix -b y -ba y -z y -o $fmapDir -f ${i}_ses-${session}_dir-${blipEnc}_run-${pos}_phasediff ${dataDir}/${blipDir}*/
+			fi
+
+			## append Json for intended use
+			fmapJson=${fmapDir}/${i}_ses-${session}_dir-${blipEnc}_run-${pos}_phasediff.json
+			intendExist=$(cat $fmapJson | jq '.IntendedFor')
+			if [ "$intendExist" == "null" ]; then
+
+				# build list of phase runs
+				runningC=$(($runningC+${expPhaseRuns[$k]}))
+				unset subjArr
+				run=1; while [ $run -le $runningC ]; do
+					arrPos=$(($run-1))
+					subjArr[$arrPos]=func/${i}_task-${task}_run-${run}_bold.nii.gz
+					let run+=1
+				done
+
+				printf '%s\n' "${subjArr[@]}" | jq -R . > ${derivDir}/tmpSubj.json
+				jq --slurpfile jsonSubjArr ${derivDir}/tmpSubj.json '. |= . + {"IntendedFor":$jsonSubjArr}' $fmapJson > ${derivDir}/tmpIntendAdd.json
+				rm $fmapJson && mv ${derivDir}/tmpIntendAdd.json $fmapJson
 			fi
 		done
 	fi
